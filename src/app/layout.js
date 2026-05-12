a528a66e940fbfaac81f57ebea865328f222d248
OmitHasanAdor <ibneshams05@gmail.com>
2026-05-13 04:11:16 +0600

    fixing animate css for my project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
-import 'animate.css';
+
+
 import "./globals.css";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ToastContainer } from "react-toastify";
@@ -24,6 +25,13 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+
+      <head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+</head>
 
       <body className="min-h-full flex flex-col">
         {children}
