f830229d315cabab9b65ecd284de278ee0108c72
OmitHasanAdor <ibneshams05@gmail.com>
2026-05-14 04:50:30 +0600

    routing related issue fixing in router

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -3,7 +3,7 @@ import { authClient } from '@/lib/auth-client';
 // import Image from 'next/image';
 import ProfileAnimation from "@/components/ProfileAnimation";
 import Link from 'next/link';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 import { useForm } from "react-hook-form";
 import { FaGoogle } from 'react-icons/fa';
@@ -15,7 +15,7 @@ const LoginPage = () => {
 
 
 
-    //  const router = useRouter()
+     const router = useRouter()
     const redirectPath = useSearchParams().get("redirect") || "/"
 
     const handleGoogleSign = async () => {
