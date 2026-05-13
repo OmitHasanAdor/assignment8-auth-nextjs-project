f503c03ffbeeb346176cece4566c7d4e89e8b420
OmitHasanAdor <ibneshams05@gmail.com>
2026-05-13 23:44:18 +0600

    solving responsiveness

--- src/app/(main)/myprofile/page.jsx
+++ src/app/(main)/myprofile/page.jsx
@@ -47,7 +47,7 @@ const MyProfilePage = () => {
 <div className="relative z-10 flex-1 flex justify-center items-center min-h-[80vh] w-auto shadow-md rounded-md border-2 border-gray-200 mx-auto ">
    <ProfileAnimation 
    fileName="Security.lottie"
-       className="min-h-[80vh] w-auto"
+       className=" max-w-[80%] h-auto"
    />
 </div>
 
